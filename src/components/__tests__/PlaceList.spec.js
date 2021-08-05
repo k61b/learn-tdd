beb02dae5d253f9c1e8b5258500ff51a52be1827
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-08-05 22:48:21 +0300

    Update PlaceList test more effective

--- src/components/__tests__/PlaceList.spec.js
+++ src/components/__tests__/PlaceList.spec.js
@@ -2,24 +2,25 @@ import { render } from "@testing-library/react";
 import { PlaceList } from "../PlaceList";
 
 describe("PlaceList", () => {
+  const places = [
+    { id: 1, name: "Sushi Place" },
+    { id: 2, name: "Pizza Place" },
+  ];
+  let loadPlaces;
+  let context;
+
+  beforeEach(() => {
+    loadPlaces = jest.fn().mockName("loadPlaces");
+
+    context = render(<PlaceList loadPlaces={loadPlaces} places={places} />);
+  });
+
   it("loads places on first render", () => {
-    const loadPlaces = jest.fn().mockName("loadPlaces");
-    const places = [];
-
-    render(<PlaceList loadPlaces={loadPlaces} places={places} />);
-
     expect(loadPlaces).toHaveBeenCalled();
   });
 
   it("displays the places", () => {
-    const noop = () => {};
-    const places = [
-      { id: 1, name: "Sushi Place" },
-      { id: 2, name: "Pizza Place" },
-    ];
-    const { queryByText } = render(
-      <PlaceList loadPlaces={noop} places={places} />,
-    );
+    const { queryByText } = context;
 
     expect(queryByText("Sushi Place")).not.toBeNull();
     expect(queryByText("Pizza Place")).not.toBeNull();
