45c0a99bea402201385ad60d05ff23f97422722a
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-08-05 22:07:06 +0300

    Add display the places test

--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 
-export function PlaceList({ loadPlaces }) {
+export function PlaceList({ loadPlaces, places }) {
   useEffect(() => {
     loadPlaces();
   }, [loadPlaces]);
-  return <div>PlaceList</div>;
+  return (
+    <ul>
+      {places.map(place => (
+        <li key={place.id}>{place.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default PlaceList;
--- src/components/__tests__/PlaceList.spec.js
+++ src/components/__tests__/PlaceList.spec.js
@@ -4,9 +4,24 @@ import { PlaceList } from "../PlaceList";
 describe("PlaceList", () => {
   it("loads places on first render", () => {
     const loadPlaces = jest.fn().mockName("loadPlaces");
+    const places = [];
 
-    render(<PlaceList loadPlaces={loadPlaces} />);
+    render(<PlaceList loadPlaces={loadPlaces} places={places} />);
 
     expect(loadPlaces).toHaveBeenCalled();
+  });
+
+  it("displays the places", () => {
+    const noop = () => {};
+    const places = [
+      { id: 1, name: "Sushi Place" },
+      { id: 2, name: "Pizza Place" },
+    ];
+    const { queryByText } = render(
+      <PlaceList loadPlaces={noop} places={places} />,
+    );
+
+    expect(queryByText("Sushi Place")).not.toBeNull();
+    expect(queryByText("Pizza Place")).not.toBeNull();
   });
 });
